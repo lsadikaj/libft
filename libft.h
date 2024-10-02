d64f7a6e24896870dcd568be75aba2afbd06ed5d
lsadikaj <leotrimsad@gmail.com>
2024-10-02 13:38:47 +0200

    add some files

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsadikaj <lsadikaj@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 12:54:17 by lsadikaj          #+#    #+#             */
+/*   Updated: 2024/10/02 10:39:13 by lsadikaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+#endif
