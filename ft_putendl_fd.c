d17512d694b6cf6a21bbed262d95d0fb79fa5b0f
Leotrim Sadikaj <leotrimsadikaj@macbook-pro-de-leotrim.home>
2024-10-09 22:09:37 +0200

    homeJ9

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,40 +6,41 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 08:50:08 by lsadikaj          #+#    #+#             */
-/*   Updated: 2024/10/07 08:59:31 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2024/10/09 21:58:27 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "libft.h"
 #include <unistd.h>
 
-void    ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-    if (!s)
-        return;
-    while (*s)
-        write (fd, s++, 1);
-    write (fd, "\n", 1);
+	if (!s)
+		return ;
+	while (*s)
+		write (fd, s++, 1);
+	write (fd, "\n", 1);
 }
-
+/*
 #include <fcntl.h> // Pour open
 
-int main(void)
+int	main(void)
 {
-    int fd;
+	int	fd;
 
-    // Test 1: Écrire dans la sortie standard (écran)
-    ft_putendl_fd("Hello, World!", 1);
+	// Test 1: Écrire dans la sortie standard (écran)
+	ft_putendl_fd("Hello, World!", 1);
 
-    // Test 2: Écrire dans un fichier
-    fd = open("test_output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd < 0)
-    {
-        write(1, "Error: Unable to open file\n", 28);
-        return 1;
-    }
-    ft_putendl_fd("This is a test string in the file.", fd);
-    close(fd);
+	// Test 2: Écrire dans un fichier
+	fd = open("test_output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+	{
+		write(1, "Error: Unable to open file\n", 28);
+		return (1);
+	}
+	ft_putendl_fd("This is a test string in the file.", fd);
+	close(fd);
 
-    return 0;
+	return (0);
 }
+*/
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,41 +6,41 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 13:34:42 by lsadikaj          #+#    #+#             */
-/*   Updated: 2024/10/06 22:02:48 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2024/10/09 21:03:58 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    unsigned int i;
+	unsigned int	i;
 
-    if (!s || !f)
-        return;
-
-    i = 0;
-    while (s[i])
-    {
-        f(i, &s[i]);
-        i++;
-    }
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
-
+/*
 #include <stdio.h>
 #include <ctype.h>
 
-void to_upper(unsigned int index, char *c)
+void	to_upper(unsigned int index, char *c)
 {
-    (void)index;
-    *c = (char)toupper(*c);
+	(void)index;
+	*c = (char)toupper(*c);
 }
 
-int main(void)
+int	main(void)
 {
-    char str[] = "hello world";
-    ft_striteri(str, to_upper);
-    printf("Modified string: '%s'\n", str); // Should print 'HELLO WORLD'
+	char	str[] = "hello world";
+	ft_striteri(str, to_upper);
+	printf("Modified string: '%s'\n", str); // Should print 'HELLO WORLD'
 
-    return 0;
+	return (0);
 }
+*/
